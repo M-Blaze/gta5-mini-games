1d55c05d8f2e65cc8d4e8b35dec5199c1f7315fc
Moulik Rai <raimoulik@gmail.com>
2023-07-19 12:25:30 +0545

    refactor!: move all components near the parent component in Bank Hack game

--- src/app/(games)/bank-hack/page.tsx
+++ src/app/(games)/bank-hack/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react'
 
-import BankHack from './bankHack'
+import BankHack from './BankHack'
 import { BankHackContext } from './BankHackContext'
 
 const DEFAULT_STATE = {
