aa052063dd5a5925b7d5ff1d7f30e6e865764a74
Moulik Rai <raimoulik@gmail.com>
2023-07-20 01:54:22 +0545

    chore: add Codes component

---
+++ src/app/(games)/bank-hack/components/Codes.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import CharacterBox from './CharacterBox'
+import { Coordinates } from '../bankHack.types'
+
+interface CodesProps {
+  codes: string[],
+  classNames?: string,
+  coordinates?: Coordinates
+}
+
+const Codes:React.FC<CodesProps> = ({ codes, classNames, coordinates }) => {
+  const top = (coordinates?.y || 0) * 43 + 'px'
+  const left = (coordinates?.x || 0) * 43  + 'px'
+
+  return (
+    <div className={`codes flex z-10 ${classNames} absolute`} style={{ left, top }}>
+      {
+        codes.map((code, index) => <CharacterBox character={code} key={index} />)
+      }
+    </div>
+  )
+}
+
+export default Codes
