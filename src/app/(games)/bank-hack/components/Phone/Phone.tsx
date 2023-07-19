6d603cc5b1dbe73dcbf29d6a5d0d5899e11eed07
Moulik Rai <raimoulik@gmail.com>
2023-07-19 10:47:25 +0545

    refactor: phone component

--- src/app/(games)/bank-hack/components/Phone/Phone.tsx
+++ src/app/(games)/bank-hack/components/Phone/Phone.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
+import { BankHackContext } from '../../BankHackContext'
 import Speaker from './components/Speaker'
-import Camera from './components/Camera'
-import Screen from './components/Screen'
+import Timer from './components/Timer'
+import Characters from './components/Characters'
+import Loader from './components/Loader'
 
+const GAME_STATE = ''
 const Phone = () => {
   return (
     <div className='phone bg-zinc-800 relative' style={{ height: '484px', width: '967px', borderRadius: '50px' }}>
       <Speaker className='left-8'/>
       <Speaker className='right-8'/>
-      <Camera />
-      <Screen />
+      <div className="camera w-4 h-4 bg-zinc-900 rounded-full absolute left-8 top-3/4"></div>
+      <div className="screen absolute top-6 bottom-6 left-24 right-24 bg-black">
+        {
+          GAME_STATE === '' ? <Loader /> :
+          (<>
+            <Timer />
+            <Characters />
+          </>)
+        }
+      </div>
     </div>
   )
 }
