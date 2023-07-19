45f2d76a805eb886598501bec6d61a7be0cfcc2d
Moulik Rai <raimoulik@gmail.com>
2023-07-19 10:23:11 +0545

    refactor: remove unwanted components

--- src/app/(games)/bank-hack/components/Phone/components/Camera.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Camera = () => {
-  return (
-    <div className="camera w-4 h-4 bg-zinc-900 rounded-full absolute left-8 top-3/4"></div>
-  )
-}
-
-export default Camera
