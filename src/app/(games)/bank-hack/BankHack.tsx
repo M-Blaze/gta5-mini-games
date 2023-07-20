c80c070e40fcf526bdd2a4fae2a866012758aaef
Moulik Rai <raimoulik@gmail.com>
2023-07-20 10:36:07 +0545

    chore: add settings component

--- src/app/(games)/bank-hack/BankHack.tsx
+++ src/app/(games)/bank-hack/BankHack.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image'
-import { BiSolidCog } from 'react-icons/bi'
 import { VscDebugStart } from 'react-icons/vsc'
 
 import { generateCharacter } from './helpers/bank-hack.helpers'
@@ -15,6 +14,7 @@ import Loader from './components/Loader'
 import Codes from './components/Codes'
 import LeftCode from './components/LeftCode'
 import RightCode from './components/RightCode'
+import SettingsModal from './components/SettingsModal'
 
 type HackStates = '' | 'hack-started' | 'hacking' | 'failure' | 'success'
 type Coordinates = {
@@ -84,6 +84,7 @@ const BankHack = () => {
   }
 
   const failedToHack = () => {
+    setSuccessCount(0)
     setHackState('failure')
   }
   
@@ -103,9 +104,9 @@ const BankHack = () => {
   }
 
   return (
-    <div className='game__bank-hack'>
+    <div className='game__bank-hack relative'>
       <Image src='/bank-hack-bg.jpg' alt='bank hack background image' className='-z-10' fill />
-      <div className="h-screen flex justify-center items-center">
+      <div className="min-h-screen flex justify-center items-center">
         <div className='phone bg-zinc-800 relative' style={{ height: '476px', width: '967px', borderRadius: '50px' }}>
           <Speaker className='left-8'/>
           <Speaker className='right-8'/>
@@ -115,9 +116,7 @@ const BankHack = () => {
               (hackState === '' || hackState === 'failure' || hackState === 'success') && 
               (
                 <div className="actions-block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center text-center z-10">
-                  <div className="button-holder hover:bg-red-500 border-2 border-red-500 py-2 px-4 rounded-md mr-4">
-                    <button className='flex items-center'><BiSolidCog className='mr-1' /> Settings</button>
-                  </div>
+                  <SettingsModal />
                   <div className="button-holder">
                     <button className='hover:bg-red-500 border-2 border-red-500 py-2 px-4 rounded-md flex items-center' onClick={startHack}><VscDebugStart className='mr-1' /> Start</button>
                   </div>
