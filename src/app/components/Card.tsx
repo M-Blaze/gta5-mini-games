2007b2ab356bfcdee0e66bf09dc9f292ac29cb17
Moulik Rai <raimoulik@gmail.com>
2023-07-20 08:27:48 +0545

    update: card styling

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,12 +8,12 @@ interface CardProps {
 
 const Card:React.FC<CardProps> = ({ imageUrl, title }) => {
   return (
-    <div className='card text-center'>
+    <div className='card text-center bg-gray-400 p-4 rounded-md'>
       <div className="image-holder relative mb-2" style={{ paddingTop: '50%' }}>
         <Image src={imageUrl} fill alt='game image' />
       </div>
       <div className="text-wrap">
-        <h3>{ title }</h3>
+        <h3 className='font-bold'>{ title }</h3>
       </div>
     </div>
   )
