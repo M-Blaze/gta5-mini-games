29a013783650c401e25c3b99d87e83afec50dd43
Moulik Rai <raimoulik@gmail.com>
2023-07-20 08:36:55 +0545

    update: show only two digit for milliseconds in timer

--- src/app/(games)/bank-hack/components/Timer.tsx
+++ src/app/(games)/bank-hack/components/Timer.tsx
@@ -8,8 +8,8 @@ interface TimerProps {
 const INTERVAL = 20
 const Timer:React.FC<TimerProps> = ({ onFailure }) => {
   const [remainingMilliSeconds, setRemainingMilliSeconds] = useState(15000)
-  const seconds = Math.floor((remainingMilliSeconds % 60000) / 1000);
-  const milliSeconds = remainingMilliSeconds % 1000;
+  const seconds = Math.floor((remainingMilliSeconds % 60000) / 1000)
+  const milliSeconds = (remainingMilliSeconds % 1000) / 10
 
   useEffect(() => {
     let remainingMilliseconds = remainingMilliSeconds;
@@ -33,7 +33,7 @@ const Timer:React.FC<TimerProps> = ({ onFailure }) => {
   }, [])
 
   return (
-    <span className='absolute top-2 left-1/2 -translate-x-1/2 text-lime-500 text-2xl font-bold'>{`${seconds.toString().padStart(2, '0')}.${milliSeconds.toString().padStart(2, '0')}`} s</span>
+    <span className='absolute top-2 left-1/2 -translate-x-1/2 text-lime-500 text-2xl font-bold'>{`${seconds.toString().padStart(2, '0')}.${milliSeconds.toString().padStart(2, '0')}`}s</span>
   )
 }
 
